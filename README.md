afaf06840321d94055f5c56bb683d9a4d2dea6e3
luissmga <luisrolfo1234@gmail.com>
2024-02-13 11:38:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# practica_basic_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/practica_basic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practica_basic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
