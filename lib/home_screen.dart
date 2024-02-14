f09d884c0a90418f54027bcf61dc0975830dba3e
luissmga <luisrolfo1234@gmail.com>
2024-02-14 10:46:17 -0600

    Adding home_screen.dart

---
+++ lib/home_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HomeScreeen extends StatelessWidget {
+  const HomeScreeen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
